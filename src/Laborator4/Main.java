4b4e625d49b1a7cbd2cddd73cd939af169dc35ac
Alexandru Cerghezan <cerghezan2005@gmail.com>
2026-03-31 02:00:42 +0300

    “Tema Laborator 4”

--- src/Laborator4/Main.java
+++ src/Laborator4/Main.java
@@ -6,6 +6,18 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
+
+    public static double gasesteNota(String prenume, String nume, HashMap<String, Student> map) {
+
+        String cheie = prenume + " " + nume;
+        Student studentGasit = map.get(cheie);
+
+        if (studentGasit != null) {
+            return studentGasit.nota;
+        }
+        return 0.0;
+    }
+
     public static void main(String[] args) {
 
         HashMap<Integer, Student> mapStudenti = new HashMap<>();
@@ -19,11 +31,9 @@ public class Main {
                 String[] date = linie.split(",");
 
                 Student studentNou = new Student(date[0], date[1], date[2], date[3]);
-
                 mapStudenti.put(studentNou.numarMatricol, studentNou);
             }
             scannerStudenti.close();
-
         } catch (FileNotFoundException e) {
             System.out.println(" Nu am gasit fisierul studenti.txt ");
         }
@@ -40,20 +50,32 @@ public class Main {
                 double nota = Double.parseDouble(date[1]);
 
                 Student studentGasit = mapStudenti.get(matricol);
-
                 if (studentGasit != null) {
                     studentGasit.setNota(nota);
                 }
             }
             scannerNote.close();
-
         } catch (FileNotFoundException e) {
             System.out.println(" Nu am gasit fisierul note_anon.txt");
         }
 
-        System.out.println("Lista studentilor cu notele actualizate este:");
+
+        System.out.println("Lista studentilor cu note actualizate:");
         for (Student s : mapStudenti.values()) {
             System.out.println(s.toString());
         }
+
+        //Tema Laborator4:
+
+        HashMap<String, Student> mapDupaNume = new HashMap<>();
+        for (Student s : mapStudenti.values()) {
+            mapDupaNume.put(s.prenume + " " + s.nume, s);
+        }
+
+        double notaM = gasesteNota("Bianca", "Popescu", mapDupaNume);
+        double notaN = gasesteNota("Ioan", "Popa", mapDupaNume);
+
+        System.out.println("\n Ce nota are Bianca Popescu: " + notaM);
+        System.out.println(" Ce nota are Ioan Popa: " + notaN);
     }
 }
