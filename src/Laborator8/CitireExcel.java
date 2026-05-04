b4cc01e762330d8364d632e0de3b52022a9f7690
Alexandru Cerghezan <cerghezan2005@gmail.com>
2026-05-04 18:38:56 +0300

    Laborator8 : 8.5.2

--- src/Laborator8/CitireExcel.java
+++ src/Laborator8/CitireExcel.java
@@ -1,4 +1,4 @@
-package Laborator8;
+package     Laborator8;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -7,33 +7,25 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
 
 public class CitireExcel {
 
     public static void main(String[] args) {
+        System.out.println("--- Datele din fișierul inițial ---");
         try {
-            // 1. Deschidem fișierul Excel
             FileInputStream file = new FileInputStream(new File("C:\\Users\\Alexandru Cerghezan\\IdeaProjects\\Ingineria-Sistemelor\\src\\Laborator8\\laborator8_input.xlsx"));
-
-            // 2. Creăm instanța workbook-ului (specific pentru .xlsx)
             XSSFWorkbook workbook = new XSSFWorkbook(file);
-
-            // 3. Obținem prima pagină din Excel (index 0)
             XSSFSheet sheet = workbook.getSheetAt(0);
 
-            // 4. Iterăm prin fiecare rând din pagină
             Iterator<Row> rowIterator = sheet.iterator();
             while (rowIterator.hasNext()) {
                 Row row = rowIterator.next();
-
-                // 5. Iterăm prin celulele de pe rândul curent
                 Iterator<Cell> cellIterator = row.cellIterator();
                 while (cellIterator.hasNext()) {
                     Cell cell = cellIterator.next();
-
-                    // 6. Afișăm în consolă în funcție de tipul de date (Numeric sau String)
                     switch (cell.getCellType()) {
                         case NUMERIC:
                             System.out.print(cell.getNumericCellValue() + " \t\t ");
@@ -47,15 +39,61 @@ public class CitireExcel {
                 }
                 System.out.println();
             }
-
             file.close();
             workbook.close();
 
         } catch (IOException e) {
-            System.err.println("Eroare: Nu am putut găsi sau citi fișierul Excel.");
-            System.err.println("Asigură-te că 'laborator8_input.xlsx' este în folderul ProiectareSoftware!");
+            e.printStackTrace();
+        }
+        genereazaFisierCuMedie();
+
+    }
+
+    public static void genereazaFisierCuMedie() {
+        try {
+            FileInputStream fileIn = new FileInputStream(new File("C:\\Users\\Alexandru Cerghezan\\IdeaProjects\\Ingineria-Sistemelor\\src\\Laborator8\\laborator8_input.xlsx"));
+            XSSFWorkbook workbook = new XSSFWorkbook(fileIn);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            for (int i = 0; i <= sheet.getLastRowNum(); i++) {
+                Row row = sheet.getRow(i);
+
+                if (row == null) {
+                    continue;
+                }
+
+                Cell newCell = row.createCell(6);
+
+                if (i == 0) {
+                    newCell.setCellValue("Media");
+                } else {
+
+                    Cell cellNota1 = row.getCell(3);
+                    Cell cellNota2 = row.getCell(4);
+                    Cell cellNota3 = row.getCell(5);
+
+                    double n1 = (cellNota1 != null) ? cellNota1.getNumericCellValue() : 0;
+                    double n2 = (cellNota2 != null) ? cellNota2.getNumericCellValue() : 0;
+                    double n3 = (cellNota3 != null) ? cellNota3.getNumericCellValue() : 0;
+
+                    double media = (n1 + n2 + n3) / 3.0;
+                    newCell.setCellValue(media);
+                }
+            }
+
+            fileIn.close();
+
+            FileOutputStream fileOut = new FileOutputStream(new File("C:\\\\Users\\\\Alexandru Cerghezan\\\\IdeaProjects\\\\Ingineria-Sistemelor\\\\laborator8_output2.xlsx"));
+            workbook.write(fileOut);
+
+            fileOut.close();
+            workbook.close();
+
+            System.out.println("SUCCES! Fișierul 'laborator8_output2.xlsx' a fost generat.");
+
+        } catch (IOException e) {
+            System.err.println("A apărut o eroare la crearea noului fișier.");
             e.printStackTrace();
         }
     }
 }
-
