ec39f23116fd708dd40b0cf38e869ebf20425252
Alexandru Cerghezan <cerghezan2005@gmail.com>
2026-05-25 22:06:59 +0300

    Laborator11 11.3.1

---
+++ src/Laborator11/MediaInterested.java
@@ -0,0 +1,17 @@
+package Laborator11;
+
+public class MediaInterested implements Observer {
+    private String name;
+
+    public MediaInterested(String name) {
+        this.name = name;
+    }
+
+    public void doSomeLogic(String message) {
+        System.out.println("["+name + "] received update: " + message);
+    }
+
+    public void update(String message){
+        doSomeLogic(message);
+    }
+}
---
+++ src/Laborator11/YouTubeChannel.java
@@ -0,0 +1,34 @@
+package Laborator11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class YouTubeChannel implements Subject{
+    private String channelName;
+
+    private List<Observer> observers= new ArrayList<>();
+    public YouTubeChannel(String channelName) {
+        this.channelName = channelName;
+    }
+
+    public void uploadVideo(String title) {
+        System.out.println("{"+ channelName + "} uploaded a new video: " + title);
+        notifyObservers(title);
+    }
+
+
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+
+    public void notifyObservers(String message) {
+        for (Observer observer: observers){
+            observer.update(message);
+        }
+    }
+}
